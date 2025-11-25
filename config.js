ac040fa8bcafb511450622090cd67b268a9a8f01
Anaru <eivindrosstadskeie@gmail.com>
2025-11-25 23:48:56 +0100

    Remove all application source code, configuration, documentation, and styling.

--- config.js
+++
@@ -1,9 +0,0 @@
-// --- FIREBASE CONFIGURATION ---
-export const firebaseConfig = {
-    apiKey: "AIzaSyD-E58BdRwFCHDmsgRG9-PEWzMaQoheJ-0",
-    authDomain: "roesstad-9fd48.firebaseapp.com",
-    projectId: "roesstad-9fd48",
-    storageBucket: "roesstad-9fd48.firebasestorage.app",
-    messagingSenderId: "704119736902",
-    appId: "1:704119736902:web:bc47f2b6a1383de0479847"
-};
